89f954de5e8d5aa474734b365871b343f378f3c9
Brian Peterson <bpeterson1579@gmail.com>
2022-02-19 11:21:33 -0700

    Clean up directory

---
+++ src/Components/App.tsx
@@ -0,0 +1,18 @@
+import '../CSS/App.css';
+import React from 'react';
+
+type pokemon = {pokemon: []}
+
+class App extends React.Component <pokemon, {}> {
+  state = {pokemon: []}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
