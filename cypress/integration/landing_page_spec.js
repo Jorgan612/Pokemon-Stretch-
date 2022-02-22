6c17fdf5d3bf5114ac5f1ea04d9426171f8d643f
Susanna Carey <susannaopal@gmail.com>
2022-02-22 18:54:33 -0500

    Write initial tests for landing page user flows

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -1,8 +1,18 @@
-beforeEach(() => {
-    cy.visit('http://localhost:3000');
+describe('Landing Page User Flows', () => {
+  beforeEach(() => {
+     cy.visit('http://localhost:3000');
   });
-  describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+
+  it('should be able to visit the url', () => {
+    cy.url()
+    .should('eq', 'http://localhost:3000/');
+  });
+
+  it('should be able to see a nav with the app\'s title', () => {
+    cy.get('.nav-div')
+    .get('.title')
+    .contains('Gotta Ketchum All')
+    .should('be.visible')
+  });
+
+});
