94fca0896d2f60106e161c73309cd2f01b64f76f
Susanna Carey <susannaopal@gmail.com>
2022-02-22 20:36:14 -0500

    Fix syntax mistake on landing page tessts

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -1,6 +1,5 @@
 //Need to switch to intercept testing with stubbing and add fixtures 
 
-
 describe('Landing Page User Flows', () => {
   beforeEach(() => {
      cy.visit('http://localhost:3000');
@@ -23,32 +22,32 @@ describe('Landing Page User Flows', () => {
   .contains('Home')
   .click()
   .should('be.visible')
-  })
+  });
 
   it('should have a favorites button on the nav', () => {
   cy.get('.nav-div > :nth-child(2) > :nth-child(2)')
   .contains('Favorites')
   .click()
   .should('be.visible')
-  })
+  });
 
   it('should have a search field', () => {
     cy.get('input')
     .type('caterpie')
     .should('be.visible')
-  })
+  });
 
   it('should have a search button', () => {
     cy.get('.nav-div > :nth-child(2) > :nth-child(4)')
     .contains('Search')
     .should('be.visible')
-  })
+  });
 
   it('should be able to click a Pokeball and see a list of a specific generation\'s Pokemon', () => {
     cy.get('[href="/generation-i"]')
     .click()
     .url().should('eq', 'http://localhost:3000/generation-i')
-  })
+  });
 
 
 });
