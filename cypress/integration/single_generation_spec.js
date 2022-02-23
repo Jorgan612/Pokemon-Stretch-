22019b5e15873860f1dbc4a24f941ef933d20780
Susanna Carey <susannaopal@gmail.com>
2022-02-22 20:34:14 -0500

    Set up testing for single Pokemon view

--- cypress/integration/single_generation_spec.js
+++ cypress/integration/single_generation_spec.js
@@ -1,8 +1,14 @@
-beforeEach(() => {
-    cy.visit('http://localhost:3000');
+describe('Landing Page User Flows', () => {
+  beforeEach(() => {
+     cy.visit('http://localhost:3000');
   });
-  describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+
+
+// it('User should see a button that says Main Page and be able to click it to return to main page', () => {
+//     cy.get('button').contains('Home')
+//     cy.get('button').contains('Home').click()
+//     .url().should('eq', 'http://localhost:3000/')
+//     .get('.nav-div')
+//   })
+
+});
