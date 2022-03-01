409158c43d90834c1d56bb74e3503ac889a55ee9
Brian Peterson <bpeterson1579@gmail.com>
2022-02-28 22:03:54 -0700

    Update the styling of the button when unfavoriting

--- src/Components/PokeCard.tsx
+++ src/Components/PokeCard.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import '../CSS/PokeCard.css';
 import { Link } from 'react-router-dom';
 
-const PokeCard = ({name, sprite, id}) => {
+const PokeCard = ({name, sprite, id, favPoke}) => {
   return(
     <Link to={`${Number(id) - 1}`} className='poke-card' >
       <img className='card-sprite' src={sprite} alt='pokemon character'/>
       <p className='pokemon-name'>{name}</p>
+      {favPoke}
     </Link>
   )
 }
